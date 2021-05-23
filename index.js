bc3d9389a6543ecf5e2ed4ccc54fac6c6adeaa90
Pavel Pashov <pashov.pavel@gmail.com>
2021-05-23 20:19:02 +0300

    uibuild

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ morgan.token('body', function (req) {
 app.use(express.json())
 app.use(express.static('build'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-//herokutest
+
 let persons = [
     {
         name: "Arto Hellas",
